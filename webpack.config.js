db918004053ee1b8ab06eab0e79b5af64426a0bf
catalinbroinas <catalinbroinas@yahoo.com>
2024-03-18 17:05:42 +0200

    Implement initial webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/js/index.js',
+    mode: 'development',
+    devtool: 'source-map',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist', 'js'),
+    },
+};
