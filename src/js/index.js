bff4f99720a3f33b3d16dbe38c7cfc62afdb05fb
catalinbroinas <catalinbroinas@yahoo.com>
2024-03-20 14:52:19 +0200

    Add function `setActiveSidebarButton()` to 'UI()'  * To handle sidebar button activation

--- src/js/index.js
+++ src/js/index.js
@@ -8,9 +8,27 @@ function UI() {
     const weekButton = document.querySelector('#week-btn');
     const newProjectButton = document.querySelector('#new-project-btn');
 
+    const setActiveSidebarButton = (getActiveButton) => {
+        const navItems = document.querySelectorAll('nav-item');
+        const setActiveItem = document.querySelector(`#${getActiveButton}`);
+
+
+        navItems.forEach((item) => {
+            if (item.classList.contains('active')) {
+                item.classList.remove('active');
+            }
+        });
+
+        if (setActiveItem) {
+            setActiveItem.classList.add('active');
+        }
+    };
+
     const displayInbox = () => {
         const inbox = inboxDOM();
         const inboxAddTask = inbox.addTask();
+
+        setActiveSidebarButton('inbox-btn');
 
         pageContent.appendChild(inboxAddTask);
     };
