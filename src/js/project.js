3205d554014a32a5a473f53d984022ea27bae15f
catalinbroinas <catalinbroinas@yahoo.com>
2024-03-20 19:22:16 +0200

    Implement `clickOnButton()` to `projectDOM`  * Create a form for set the name of a new project

---
+++ src/js/project.js
@@ -0,0 +1,42 @@
+function projectDOM() {
+    const clickOnButton = () => {
+        const form = document.createElement('form');
+        const input = document.createElement('input');
+        const groupButtons = document.createElement('div');
+        const addButton = document.createElement('button');
+        const addButtonIcon = document.createElement('i');
+        const cancelButton = document.createElement('button');
+        const cancelButtonIcon = document.createElement('i');
+
+        form.classList.add('new-project-form');
+        input.classList.add('form-control');
+        addButton.classList.add('add-btn');
+        addButtonIcon.classList.add('mdi', 'mdi-send', 'action-btn-icon');
+        cancelButton.classList.add('cancel-btn');
+        cancelButtonIcon.classList.add('mdi', 'mdi-cancel', 'action-btn-icon');
+
+        input.setAttributeNS('type', 'text');
+        input.setAttribute('id', 'set-name-project');
+        input.setAttribute('name', 'name-project');
+        input.setAttribute('placeholder', 'Name');
+        addButton.setAttribute('type', 'button');
+        cancelButton.setAttribute('type', 'button');
+
+        form.appendChild(input);
+        form.appendChild(groupButtons);
+        groupButtons.appendChild(addButton);
+        addButton.appendChild(addButtonIcon);
+        addButton.insertAdjacentText('beforeend', 'Add');
+        groupButtons.appendChild(cancelButton);
+        cancelButtonIcon.appendChild(cancelButtonIcon);
+        cancelButton.insertAdjacentText('beforeend', 'Cancel');
+
+        return form;
+    };
+
+    return {
+        clickOnButton
+    };
+}
+
+export { projectDOM };
