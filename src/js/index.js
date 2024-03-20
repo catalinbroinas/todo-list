8963eef71f18aa4ab4793cf7dded94dbe17be28d
catalinbroinas <catalinbroinas@yahoo.com>
2024-03-20 14:32:56 +0200

    Implement `UI()` for display DOM  * Add 'displayInbox()' for display inbox content

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,26 @@
 import '../css/style.css';
 import { inboxDOM } from './inbox';
+
+function UI() {
+    const pageContent = document.querySelector('#content');
+    const inboxButton = document.querySelector('#inbox-btn');
+    const todayButton = document.querySelector('#today-btn');
+    const weekButton = document.querySelector('#week-btn');
+    const newProjectButton = document.querySelector('#new-project-btn');
+
+    const displayInbox = () => {
+        const inbox = inboxDOM();
+        const inboxAddTask = inbox.addTask();
+
+        pageContent.appendChild(inboxAddTask);
+    };
+
+    return {
+        displayInbox
+    };
+}
+
+window.addEventListener('load', () => {
+    const ui = UI();
+    ui.displayInbox();
+});
