8ace6058cce322f764e6a0cd81f18757ab8eed7c
catalinbroinas <catalinbroinas@yahoo.com>
2024-04-05 14:00:54 +0300

    Implement `pageContent`: update task list on page

--- js/main.js
+++ js/main.js
@@ -1432,6 +1432,26 @@ function projectDOM() {
         }
     };
 
+    const pageContent = (projectName) => {
+        const pageContent = document.querySelector('#content');
+        const pageTitle = utilities.addTitle(projectName);
+        const tasks = displayTasks(projectName);
+        const addTaskButton = utilities.createButton({
+            name: 'Add Task',
+            buttonClass: ['add-task-btn'],
+            iconClass: ['mdi', 'mdi-plus', 'add-task-icon'],
+            clickHandler: () => createAndOpenModal('add-task-modal', 'Add new task')
+        });
+
+        utilities.clearPageContent(pageContent);
+
+        pageContent.appendChild(pageTitle);
+        if (tasks) {
+            pageContent.appendChild(tasks);
+        }
+        pageContent.appendChild(addTaskButton);
+    }
+
     const handleSendTaskButton = (event) => {
         const button = event.target;
         const modal = button.closest('.task-modal');
@@ -1457,6 +1477,7 @@ function projectDOM() {
         projectManager.addTask(task, taskProject);
 
         closeModal(modal.id);
+        pageContent(taskProject);
     };
 
     const handleCancelTaskButton = (event) => {
@@ -1521,7 +1542,8 @@ function projectDOM() {
         sidebarContent,
         createTaskModal,
         openModal,
-        displayTasks
+        displayTasks,
+        pageContent
     };
 }
 
